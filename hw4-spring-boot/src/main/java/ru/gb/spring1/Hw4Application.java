3f0a91d8efcce4e27d33d1e8ffa187ee1e6ff994
akarmanov <arseniy.karmanov@mail.ru>
2022-09-03 20:33:55 +0700

    Initial commit

---
+++ hw4-spring-boot/src/main/java/ru/gb/spring1/Hw4Application.java
@@ -0,0 +1,11 @@
+package ru.gb.spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw4Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Hw4Application.class, args);
+  }
+}
